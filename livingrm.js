fad497ff0e08e8c22b4f32fc37bcc934465efbdd
YellowNoah5 <103080637+YellowNoah5@users.noreply.github.com>
2023-01-26 19:49:50 -0800

    Add files via upload

---
+++ livingrm.js
@@ -0,0 +1,43 @@
+img = ""
+
+function preload()
+{
+    img = loadImage("livingrm.jpg")
+}
+
+function setup()
+{
+    canvas = createCanvas(800, 600)
+    canvas.position(620, 220)
+
+    objm = ml5.objectDetector('object', modeLoaded)
+    document.getElementById("statuss").innerHTML = "Detecting Objects"
+}
+
+function modeLoaded()
+{
+    console.log("Model Loaded!")
+
+    status = true
+
+    objm.detect(img, gotResults)
+
+}
+
+function gotResults(error, results)
+{
+    if(error) 
+    {
+        console.log(error)
+    }
+    else
+    {
+        console.log(results)
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 800, 600)
+
+}
